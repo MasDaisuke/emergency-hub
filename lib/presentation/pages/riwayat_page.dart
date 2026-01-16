faaa4e17ed77ae5482e362a227b6297a494d2c10
Mas Daisuke <rasendriyo0408@gmail.com>
2026-01-17 01:21:32 +0700

    add front riwayat page

---
+++ lib/presentation/pages/riwayat_page.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/emergency_provider.dart';
+
+class RiwayatPage extends StatefulWidget {
+  @override
+  _RiwayatPageState createState() => _RiwayatPageState();
+}
+
+class _RiwayatPageState extends State<RiwayatPage> {
+  @override
+  void initState() {
+    super.initState();
+    Future.microtask(
+      () =>
+          Provider.of<EmergencyProvider>(context, listen: false).fetchRiwayat(),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Riwayat Laporan")),
+      body: Consumer<EmergencyProvider>(
+        builder: (context, provider, child) {
+          if (provider.isLoading) {
+            return Center(child: CircularProgressIndicator());
+          }
+          if (provider.laporanList.isEmpty) {
+            return Center(child: Text("Belum ada laporan"));
+          }
+          return ListView.builder(
+            itemCount: provider.laporanList.length,
+            itemBuilder: (context, index) {
+              //
+            },
+          );
+        },
+      ),
+    );
+  }
+}
