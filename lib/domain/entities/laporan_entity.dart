4799893ab63cd2f0f170793587cfc500d8c27f62
Mas Daisuke <rasendriyo0408@gmail.com>
2026-01-16 22:35:38 +0700

    add core error &  entities laporan

---
+++ lib/domain/entities/laporan_entity.dart
@@ -0,0 +1,15 @@
+class LaporanEntity {
+  final int id;
+  final String kategori;
+  final String deskripsi;
+  final String fotoPath;
+  final String tanggal;
+
+  LaporanEntity({
+    required this.id,
+    required this.kategori,
+    required this.deskripsi,
+    required this.fotoPath,
+    required this.tanggal,
+  });
+}
