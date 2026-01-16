78920d89f0890abe0a24e6d39c398c020eeaeeca
Mas Daisuke <rasendriyo0408@gmail.com>
2026-01-16 22:44:52 +0700

    add kategori entities

---
+++ lib/domain/entities/kategori_entity.dart
@@ -0,0 +1,11 @@
+class KategoriEntity {
+  final String nama;
+  final String nomor;
+  final String iconName;
+
+  KategoriEntity({
+    required this.nama,
+    required this.nomor,
+    required this.iconName,
+  });
+}
