f939b9730723b600eb894afa5634c0935bdc89b0
Mas Daisuke <rasendriyo0408@gmail.com>
2026-01-17 01:54:55 +0700

    update Main dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'data/datasources/emergency_remote_datasource.dart';
+import 'data/repositories/emergency_repository_impl.dart';
+import 'domain/usecases/get_riwayat_usecase.dart';
+import 'domain/usecases/kirim_laporan_usecase.dart';
+import 'domain/usecases/hapus_laporan_usecase.dart';
+import 'presentation/providers/emergency_provider.dart';
+import 'presentation/pages/dashboard_page.dart';
 
 void main() {
-  runApp(const MainApp());
+  final dataSource = EmergencyRemoteDataSource();
+  final repository = EmergencyRepositoryImpl(remoteDataSource: dataSource);
+  final getRiwayat = GetRiwayatUseCase(repository);
+  final kirimLaporan = KirimLaporanUseCase(repository);
+  final hapusLaporan = HapusLaporanUseCase(repository);
+
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => EmergencyProvider(
+            getRiwayatUseCase: getRiwayat,
+            kirimLaporanUseCase: kirimLaporan,
+            hapusLaporanUseCase: hapusLaporan,
+          ),
+        ),
+      ],
+      child: MyApp(),
+    ),
+  );
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Emergency App',
+      theme: ThemeData(primarySwatch: Colors.red),
+      home: DashboardPage(),
     );
   }
 }
