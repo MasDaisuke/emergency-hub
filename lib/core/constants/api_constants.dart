a102cc02a16b1390e01523c9578ce6615b95ba06
Mas Daisuke <rasendriyo0408@gmail.com>
2026-01-17 02:10:58 +0700

    update api constant & added permission

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,4 +1,5 @@
 class ApiConstants {
-  static const String baseUrl = "http://$serverIp/emergency_api"; 
+  static const String serverIp = "localhost";
+  static const String baseUrl = "http://$serverIp/emergency_api";
   static const String uploadsUrl = "$baseUrl/uploads";
 }
