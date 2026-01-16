0fffd934d73cf2bd2dea6c7b8b8ec59c35cd877c
Mas Daisuke <rasendriyo0408@gmail.com>
2026-01-16 23:53:22 +0700

    add models laporan

---
+++ lib/data/models/laporan_model.dart
@@ -0,0 +1,27 @@
+import '../../domain/entities/laporan_entity.dart';
+
+class LaporanModel extends LaporanEntity {
+  LaporanModel({
+    required int id,
+    required String kategori,
+    required String deskripsi,
+    required String fotoPath,
+    required String tanggal,
+  }) : super(
+         id: id,
+         kategori: kategori,
+         deskripsi: deskripsi,
+         fotoPath: fotoPath,
+         tanggal: tanggal,
+       );
+
+  factory LaporanModel.fromJson(Map<String, dynamic> json) {
+    return LaporanModel(
+      id: int.parse(json['id'].toString()),
+      kategori: json['kategori'],
+      deskripsi: json['deskripsi'],
+      fotoPath: json['foto_path'] ?? '',
+      tanggal: json['tanggal_lapor'],
+    );
+  }
+}
